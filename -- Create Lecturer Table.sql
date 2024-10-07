02a67836b6ef821b58519ad3c70ef6f5cea4b6a8
Kumudu Amarakoon <kumudu4746@gmail.com>
2024-10-08 05:25:16 +0530

    Create -- Create Lecturer Table.sql

---
+++ -- Create Lecturer Table.sql
@@ -0,0 +1,10 @@
+-- Create Lecturer Table
+
+-- CREATE TABLE
+--     Lecturer(
+--         LecturerId VARCHAR (10),
+--         LecName VARCHAR (50),
+--         NIC INT (15),
+--         DepID VARCHAR (10),
+--         PRIMARY KEY(LecturerID)
+--     );
