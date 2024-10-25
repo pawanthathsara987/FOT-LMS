d33b4d71a08b9b1629d168aa0f11d904be7cf06a
kumudusandeepanee <kumudu4746@gmail.com>
2024-10-25 17:28:20 +0530

    Edit student table

--- tables_kumudu.sql
+++ tables_kumudu.sql
@@ -3,8 +3,10 @@ CREATE TABLE Student (
     stu_fname VARCHAR(50),
     stu_lname VARCHAR(50), 
     stu_email VARCHAR(100),
-    stu_DOB DATE,
-    stu_Gender ENUM('M','F'),
-    stu_Mobile INT(10),
-    dep_Id CHAR(4)  
+    stu_dob DATE,
+    stu_gender ENUM('M','F'),
+    stu_mobile INT(10),
+    dep_id CHAR(4)  
 );
+
+
