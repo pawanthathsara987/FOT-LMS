925d5d230ef0645d07695588d09d23077a360bbb
Pawan Thathsara <pawanthathsara987@Gmail.com>
2024-10-25 17:29:45 +0530

    Dean table created.

--- tables.sql
+++ tables.sql
@@ -11,3 +11,13 @@ CREATE TABLE course(
     cour_status VARCHAR(50),
     cour_credit INT(1)
 );
+
+CREATE TABLE dean(
+    dean_id CHAR(4) PRIMARY KEY,
+    dean_fname VARCHAR(50),
+    dean_lname VARCHAR(50),
+    dean_email VARCHAR(50),
+    gender ENUM('M', 'F'),
+    dean_dob DATE,
+    dean_mobile INT(10)
+);
